51ce1f0b4a70aefca79628b529a4c04c9ecdf0f3
Jun <dudwns0921@gmail.com>
2021-12-03 16:45:11 +0900

    홈화면 마무리 / 상세정보창 마무리 (리뷰, 게시판 버튼 추가)/ 뭐볼까 디자인중

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,86 +1,43 @@
 import { useEffect, useState } from "react";
 
 import Menu from "../components/Menu";
+import Slider from "../components/Slider";
 
 import styles from "../css/Home.module.css";
-
-import {motion, AnimatePresence} from "framer-motion";
-
-import {Link} from "react-router-dom";
 
 function Home() {
     const [loading, setLoading] = useState(true);
     const [contents, setContents] = useState([]);
-    const getMovies = async() => {
+    const getContents = async() => {
         const json = await (await fetch("https://api.themoviedb.org/3/movie/popular?api_key=6df683327f9037c362fcff75540a2656&language=en-US&page=1"))
         .json();
         setContents(json.results);
         setLoading(false);
     }
-    useEffect(getMovies, []);
+    useEffect(getContents, []);
 
     const makeImagePath = (id, format) => {
         return `https://image.tmdb.org/t/p/${format?format: "original"}/${id}`;
     }
-    
-    const [index, setindex] = useState(0);
-    const increaseIndex = () => {
-        if(contents){
-            const totalContents = contents.length;
-            const maxIndex = Math.ceil(totalContents/offset)-1;
-            setindex((prev)=> prev===maxIndex ? 0 : prev+1);
-        }
-    }
 
-    const rowVariants = {
-        hidden : {
-            x : window.outerWidth
-        },
-        visible : {
-            x : 0
-        },
-        exit : {
-            x : -window.outerWidth
-        }
-    }
-
-    const offset = 4;
+    const ranNum = Math.floor(Math.random()*19);
 
     return (
         <div>
             {loading ? (<h1>Loading</h1>) : (
                 <div>
                     <Menu />
+                    <div className={styles.blank}></div>
                     <div
-                    style={{backgroundImage: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${makeImagePath(contents[0].backdrop_path)})`}}  
-                    className={styles.gridContainer2}>
-                        <div className={styles.titleStyle} onClick={increaseIndex}>{contents[0].title}</div>
-                        <div className={styles.overViewStyle}>{contents[0].overview}</div>
+                        style={{backgroundImage: `linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${makeImagePath(contents[ranNum].backdrop_path)})`}}  
+                        className={styles.gridContainer}>
+                        <div className={styles.titleStyle}>{contents[ranNum].title}</div>
+                        <div className={styles.overViewStyle}>{contents[ranNum].overview.length>150 ? contents[ranNum].overview.slice(0,150)+"..." : contents[ranNum].overview}</div>
                     </div>
-                    <h2>Top 20</h2>
-                        <AnimatePresence>
-                            <motion.div
-                                className={styles.gridContainer4}
-                                variants={rowVariants}
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                transition={{duration:2}}
-                                key={index}>
-                                    {contents
-                                    .slice(1)
-                                    .slice(offset*index, offset*index+offset)
-                                    .map((content)=>(
-                                        <Link to={`/detail/${content.id}`}>
-                                        <motion.div
-                                        className={styles.contentBox}
-                                        style={{backgroundImage: `url(${makeImagePath(content.backdrop_path, "w500")})`}} 
-                                        >
-                                        </motion.div>
-                                        </Link>
-                                    ))}
-                            </motion.div>
-                        </AnimatePresence>
+
+                    <Slider title="Top Rated" criteria="top_rated"/>
+                    <div className={styles.blank2}></div>
+                    <Slider title="Now Playing" criteria="now_playing"/>                           
                 </div>
             )}
         </div>
