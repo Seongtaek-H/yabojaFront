e2a3d1c9bce9559e02ba12e68408dbee29b31678
Jun <dudwns0921@gmail.com>
2021-12-10 09:37:43 +0900

    테스트 1차 시도

--- src/routes/MovieReview.js
+++ src/routes/MovieReview.js
@@ -25,24 +25,23 @@ function MovieReview() {
               `https://api.themoviedb.org/3/movie/${id}?api_key=6df683327f9037c362fcff75540a2656&language=en-US&page=1`
           )
       ).json()
+      setContent(json);
       setLoading(false);
-      setContent(json)
   }
 
   const getReviews = async () => {
-      const json = await (
-          await fetch('/buyus/readreview?reviewTitle=ironman')
-      ).json();
-      setReviews(json);
+        const json = await (
+            await fetch(`/spring/?title=${content.title}`)
+        ).json();
+        setReviews(reviews);
   }
+  useEffect(()=>{
+        getContent();
+  }, [loading]);
 
   useEffect(()=>{
-      getContent();
-  }, [])
-
-  useEffect(()=>{
-    getReviews();
-}, [])
+      getReviews();
+  }, [content]);
 
   const makeImagePath = (id, format) => {
       return `https://image.tmdb.org/t/p/${
@@ -63,10 +62,6 @@ function MovieReview() {
     setScore(e.target.value);
   }
 
-  useEffect(()=>{
-    console.log(score);
-}, [score])
-
   const textChange = (e) => {
     setText(e.target.value);
   }
@@ -75,6 +70,10 @@ function MovieReview() {
       e.preventDefault();
       postHandler();
       navigate(`/movieReview/${content.id}`);
+  }
+
+  const refresh = () => {
+    window.location.replace(`/movieReview/${content.id}`)
   }
 
 
@@ -86,91 +85,88 @@ function MovieReview() {
   }
 
   const postHandler = () => {
-    fetch('/spring/p', {
-      method : 'POST',
-      headers : {
-        'Content-Type' : 'application/json',
-        'Access-Control-Allow-Credentials' : true
-      },
-      body : JSON.stringify(body)
-    
-    })
-
+      fetch('/spring/p', {
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json',
+          'Access-Control-Allow-Credentials' : true
+        },
+        body : JSON.stringify(body)   
+      })
 }
     
 
     return (
-        <div>
-          {loading ? <h1>Loading</h1> :(
-            <div>
-              <Menu />
+      <>
+      {(loading) ? "" : <div>
+        <Menu />
+        <div className={styles.blank}></div>
+      <div>
+          <div className={styles.writeBtn} onClick={onWriteClicked}>
+          <i class="fas fa-pen-square"></i>
+          </div>
+      </div>
+        <div
+            className={styles.background}
+            style={{
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,1)), url(${makeImagePath(
+                    content.backdrop_path
+                )})`,
+            }}>
               <div className={styles.blank}></div>
-            <div>
-                <div className={styles.writeBtn} onClick={onWriteClicked}>
-                <i class="fas fa-pen-square"></i>
-                </div>
-            </div>
-              <div
-                  className={styles.background}
-                  style={{
-                      backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,1)), url(${makeImagePath(
-                          content.backdrop_path
-                      )})`,
-                  }}>
-                    <div className={styles.blank}></div>
-                      <AnimatePresence>
-                          {writeMatch ?
-                          <>
-                          <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} onClick={onOverlayClick} className={styles.overlay}></motion.div>
-                          <motion.div initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} className={styles.writeBox} style={{top:scrollY}}>
-                            <div className={styles.how}>이 작품, 어떠셨나요?</div>
-                            <div className={styles.posterBox}>
-                                <div
-                                className={styles.poster}
-                                style={{
-                                backgroundImage: `url(${makeImagePath(
-                                    content.poster_path
-                                )})`,
-                                }}
-                                >
-                            </div>
-                            </div>
-                            <form onSubmit={handletheSubmit} className={styles.formBox}>
-                                <div className={styles.rate}>
-                                    <input onClick={scoreChange} type="radio" name="rating" value="5" id="rate1" /><label for="rate1"><i class="fas fa-star"></i></label>
-                                    <input onClick={scoreChange} type="radio" name="rating" value="4" id="rate2" /><label for="rate2"><i class="fas fa-star"></i></label>
-                                    <input onClick={scoreChange} type="radio" name="rating" value="3" id="rate3" /><label for="rate3"><i class="fas fa-star"></i></label>
-                                    <input onClick={scoreChange} type="radio" name="rating" value="2" id="rate4" /><label for="rate4"><i class="fas fa-star"></i></label>
-                                    <input onClick={scoreChange} type="radio" name="rating" value="1" id="rate5" /><label for="rate5"><i class="fas fa-star"></i></label>
-                                </div>
-                                <div className={styles.write}>
-                                    <input onChange={textChange} type="text" placeholder="작품에 대한 감상을 남겨주세요."/>
-                                </div>
-                                <div>
-                                    <Button type={"submit"} text={"작성하기"}/>
-                                </div>
-                            </form>
-                          </motion.div>
-                          </>
-                          : "" }
-                      </AnimatePresence>
+                <AnimatePresence onExitComplete={refresh}>
+                    {writeMatch ?
+                    <>
+                    <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity:0}} onClick={onOverlayClick} className={styles.overlay}></motion.div>
+                    <motion.div initial={{scale:0}} animate={{scale:1}} exit={{scale:0}} className={styles.writeBox} style={{top:scrollY}}>
+                      <div className={styles.how}>이 작품, 어떠셨나요?</div>
+                      <div className={styles.posterBox}>
+                          <div
+                          className={styles.poster}
+                          style={{
+                          backgroundImage: `url(${makeImagePath(
+                              content.poster_path
+                          )})`,
+                          }}
+                          >
+                      </div>
+                      </div>
+                      <form onSubmit={handletheSubmit} className={styles.formBox}>
+                          <div className={styles.rate}>
+                              <input onClick={scoreChange} type="radio" name="rating" value="5" id="rate1" /><label for="rate1"><i class="fas fa-star"></i></label>
+                              <input onClick={scoreChange} type="radio" name="rating" value="4" id="rate2" /><label for="rate2"><i class="fas fa-star"></i></label>
+                              <input onClick={scoreChange} type="radio" name="rating" value="3" id="rate3" /><label for="rate3"><i class="fas fa-star"></i></label>
+                              <input onClick={scoreChange} type="radio" name="rating" value="2" id="rate4" /><label for="rate4"><i class="fas fa-star"></i></label>
+                              <input onClick={scoreChange} type="radio" name="rating" value="1" id="rate5" /><label for="rate5"><i class="fas fa-star"></i></label>
+                          </div>
+                          <div className={styles.write}>
+                              <input onChange={textChange} type="text" placeholder="작품에 대한 감상을 남겨주세요."/>
+                          </div>
+                          <div>
+                              <Button type={"submit"} text={"작성하기"}/>
+                          </div>
+                      </form>
+                    </motion.div>
+                    </>
+                    : "" }
+                </AnimatePresence>
 
-                    {(reviews.length>0) ? reviews.map((review)=>(
-                        <div key={review.seq_review} className={styles.reviewBox}>
-                            <div>
-                                <div className={styles.id}>{review.id}</div>
-                                <div className={styles.eval}>⭐️{review.eval}</div>
-                                <div className={styles.reviewBody}>{review.reviewBody}</div>
-                            </div>
-                            <div className={styles.gridContainer}>
-                                <div className={styles.like}><i class="fas fa-heart fa-lg"></i>좋아요</div>
-                                <div className={styles.comment}><i class="fas fa-comment-alt fa-lg"></i>댓글달기</div>
-                            </div>
-                        </div>)) : <h1 className={styles.noReview}>작성된 리뷰가 없습니다.</h1>}
-                  </div>
+              {(reviews.length>0) ? reviews.map((review)=>(
+                  <div key={review.seq_review} className={styles.reviewBox}>
+                      <div>
+                          <div className={styles.id}>{review.id}</div>
+                          <div className={styles.eval}>⭐️{review.eval}</div>
+                          <div className={styles.reviewBody}>{review.reviewBody}</div>
+                      </div>
+                      <div className={styles.gridContainer}>
+                          <div className={styles.like}><i class="fas fa-heart fa-lg"></i>좋아요</div>
+                          <div className={styles.comment}><i class="fas fa-comment-alt fa-lg"></i>댓글달기</div>
+                      </div>
+                  </div>)) : <h1 className={styles.noReview}>작성된 리뷰가 없습니다.</h1>}
             </div>
-          )}
-        </div>
+      </div>} 
+      </>
+       
     )
           }
   
