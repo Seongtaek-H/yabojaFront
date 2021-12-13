7c4d0f046645db3939864f122425e7f9a156881c
seongtaek <seongtaek@seongtaekui-MacBookPro.local>
2021-12-13 15:11:51 +0900

    calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,6 +1,20 @@
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 const Calendar = () => {
+  const today = new Date()
+  const thisYear = today.getFullYear()
+  const thisMonth = today.getMonth()
+
+  const [year, setYear] = useState(thisYear)
+  const [month, setMonth] = useState(thisMonth + 1)
+
+  const lastDate = new Date(year, month, 0).getDate()
+  const dates = []
+  for (var i = 0; i < lastDate; i++) {
+    dates.push(i + 1)
+  }
+
   const days = [
     { id: 0, day: '일' },
     { id: 1, day: '월' },
@@ -11,36 +25,50 @@ const Calendar = () => {
     { id: 6, day: '토' },
   ]
 
-  const today = new Date()
-  const currentYear = today.getFullYear()
-  const currentMonth = today.getMonth() + 1
-  const currentDate = today.getDate()
-  const fistDay = new Date(currentYear, currentMonth - 1, 1).getDay()
+  // const getLastDate = (year, month) => {
+  //   switch (month) {
+  //     case 4:
+  //     case 6:
+  //     case 9:
+  //     case 11:
+  //       return 30
+  //     case 2:
+  //       if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) return 29
+  //       else return 28
+  //     default:
+  //       return 31
+  //   }
+  // }
 
-  const getLastDay = (year, month) => {
-    switch (month) {
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        return 30
-      case 2:
-        if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) return 29
-        else return 28
-      default:
-        return 31
-    }
-  }
-
-  console.log(today)
   return (
     <div>
-      <header>달력</header>
+      <header>
+        {year}년 {month + 1}월{' '}
+      </header>
       {/* 요일 */}
+      <button
+        onClick={() => {
+          setMonth(month - 1)
+        }}
+      >
+        {' '}
+        ◀️{' '}
+      </button>
       {days.map((day) => (
         <CalendarHeader>{day.day}</CalendarHeader>
       ))}
-      {/* 달력 일자 */}
+      <button
+        onClick={() => {
+          setMonth(month + 1)
+        }}
+      >
+        {' '}
+        ▶️{' '}
+      </button>
+
+      {dates.map((a) => {
+        return <div>{a}</div>
+      })}
     </div>
   )
 }
